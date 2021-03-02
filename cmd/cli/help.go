f444f1543d732c24940d0796d96d247daf534f80
Burak Sekili <buraksekili@gmail.com>
2021-03-02 10:24:13 +0300

    updated help command output

--- cmd/cli/help.go
+++ cmd/cli/help.go
@@ -8,5 +8,10 @@ import (
 func printHelp(w io.Writer) {
 	fmt.Fprintf(w, "USAGE\n"+
 		"rsql -h, --help				: displays usage message and exits\n"+
-		"rsql -f <FNAME>, --envfile <FNAME>	: reads environment file to establish MySQL connection.\n")
+		"rsql -f <FNAME>, --envfile <FNAME>	: reads environment file to establish MySQL connection.\n"+
+		"COMMANDS\n"+
+		"add <TABLE>\t: adds data to <TABLE>\n"+
+		"info <TABLE>\t: displays the column informations of the <TABLE>\n"+
+		"display <TABLE>\t: displays the data of the <TABLE>\n"+
+		"q, exit <TABLE>\t: exits the program\n")
 }
