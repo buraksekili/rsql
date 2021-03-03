70f626422cfe706b9225fb2f9ef650ec748c2f20
Burak Sekili <buraksekili@gmail.com>
2021-03-03 23:45:02 +0300

    added help for cmd

--- cmd/client/connection.go
+++ cmd/client/connection.go
@@ -49,6 +49,13 @@ func (c *DbClient) OpenConnection() error {
 				c.showTables()
 			case "stats":
 				c.displayDBStats()
+			case "help":
+				fmt.Println("COMMANDS\n" +
+					"\tadd <TABLE>\t: adds data to <TABLE>\n" +
+					"\tinfo <TABLE>\t: displays the column informations of the <TABLE>\n" +
+					"\tdisplay <TABLE>\t: displays the data of the <TABLE>\n" +
+					"\ttables\t\t: displays available tables under the <DB> specified by user\n" +
+					"\tq, exit <TABLE>\t: exits the program")
 			case "q":
 				return nil
 			case "exit":
